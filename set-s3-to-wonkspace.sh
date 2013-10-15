af9b61748890fe6f383381eece1cccf76f12b2d9
Donald Johnson <donj@cheetah.(none)>
2013-10-14 20:48:05 -0400

    Changed databases from testapp to kchore-webdev

---
+++ set-s3-to-wonkspace.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+heroku config:set AWS_BUCKET=firehose_bucket_donj
+heroku config:set AWS_ACCESS_KEY=AKIAIS6FVEWBMDLYM57A
+heroku config:set AWS_SECRET_KEY=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
